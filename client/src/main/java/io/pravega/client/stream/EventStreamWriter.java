ec42909410894817cb47098682f396da52037fdc
Enrico Olivelli <eolivelli@apache.org>
2019-01-03 15:55:15 +0100

    Fix javadocs errors on JDK11

--- client/src/main/java/io/pravega/client/stream/EventStreamWriter.java
+++ client/src/main/java/io/pravega/client/stream/EventStreamWriter.java
@@ -62,10 +62,9 @@ public interface EventStreamWriter<Type> extends AutoCloseable {
 
     /**
      * Start a new transaction on this stream. This allows events written to the transaction be written an committed atomically.
-     * Note that transactions can only be open for {@link EventWriterConfig#getTransactionTimeoutTime()}.
+     * Note that transactions can only be open for {@link EventWriterConfig#transactionTimeoutTime}.
      * 
      * @return A transaction through which multiple events can be written atomically.
-     * @deprecated Use {@link ClientFactory#createTransactionalEventWriter(String, Serializer, EventWriterConfig)} instead
      */
     @Deprecated
     Transaction<Type> beginTxn();
@@ -74,8 +73,7 @@ public interface EventStreamWriter<Type> extends AutoCloseable {
      * Returns a previously created transaction.
      * 
      * @param transactionId The result retained from calling {@link Transaction#getTxnId()}
-     * @return Transaction object with given UUID
-     * @deprecated Use {@link ClientFactory#createTransactionalEventWriter(String, Serializer, EventWriterConfig)} instead
+     * @return Transaction object with given UUID     
      */
     @Deprecated
     Transaction<Type> getTxn(UUID transactionId);
